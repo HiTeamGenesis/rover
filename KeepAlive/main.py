adc6a4eb3d4434351392ed34cd42ca1d78a81775
cruzryan <rncb0963@gmail.com>
2024-01-18 22:23:19 -0600

    feat: added keepalive

---
+++ KeepAlive/main.py
@@ -0,0 +1,20 @@
+#Keep it as simple as possible thank you.
+#We don't want the file that watches for crashes.... to crash
+
+import subprocess
+
+if __name__ == "__main__":
+
+    #TO-DO: CHANGE THE COMMAND SO IT LAUNCHES THE BACKEND INSTEAD
+	process = subprocess.Popen(['python3.9', 'main.py'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+	stdout, stderr = process.communicate()
+	exit_code = process.wait()
+
+	print('\nRover Crashed!\n')
+	print(stdout, stderr, exit_code)
+
+	#Start katana again
+	
+    #TODO: 
+	# - ADD RESTART 
+	# - TELL ICR ROVER CRASHED
